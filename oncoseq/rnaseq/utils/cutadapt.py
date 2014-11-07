e1427c76263e2425d1630881c9da0a54719e2443
yniknafs-mctp <yniknafs@med.umich.edu>
2014-11-07 18:30:04 -0500

    template

--- oncoseq/rnaseq/utils/cutadapt.py
+++ oncoseq/rnaseq/utils/cutadapt.py
@@ -1,8 +1,32 @@
 '''
 Created on Nov 7, 2014
-
-@author: yniknafs
+@author yniknafs
 '''
 
-if __name__ == '__main__':
-    pass
+
+import os
+import sys
+import argparse
+import logging
+import collections
+
+
+
+
+
+    
+def main():
+    # parse command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG,
+                      format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    
+    
+    return 0
+
+if __name__ == '__main__': 
+    sys.exit(main())
+
